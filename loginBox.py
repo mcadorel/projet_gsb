a5d3a05c4d547c097bd38283780d669d15740c98
Maël Cadorel <mael.cadorel@laposte.net>
2014-11-23 11:26:47 +0100

    Crude (rewrite as soon as possible) login

--- loginBox.py
+++ loginBox.py
@@ -5,6 +5,7 @@
 
 from tkinter import *
 from tkinter.ttk import Combobox
+from model import Model
 
 class LoginBox(Tk):
     """ Formulaire d'authentification. """
@@ -13,28 +14,56 @@ class LoginBox(Tk):
         self.title('Authentification')
         
         # Textboxes
-        lblHost = Label(self, text='Host : ')
-        lblHost.grid(row=0, column=0, sticky=NW)
-        self.tbHost = Entry(self)
-        self.tbHost.grid(row=0, column=1, sticky=NW)
+        lblUser = Label(self, text='Utilisateur : ')
+        lblUser.grid(row=0, column=0, sticky=NW)
+        self.tbUser = Entry(self)
+        self.tbUser.grid(row=0, column=1, sticky=NW)
 
-        lblDB = Label(self, text='Database : ')
-        lblDB.grid(row=2, column=0, sticky=NW)
-        self.tbDB = Entry(self)
-        self.tbDB.grid(row=2, column=1, sticky=NW)
+        lblPW = Label(self, text='Mot de passe : ')
+        lblPW.grid(row=2, column=0, sticky=NW)
+        self.tbPW = Entry(self)
+        self.tbPW.grid(row=2, column=1, sticky=NW)
 
         # Login button
         bnLogin = Button(self,
                          text='Login',
                          command=self.login)
-        bnLogin.grid(row=4, column=1, sticky=SE)
+        bnLogin.grid(row=4, column=1, sticky=S)
+
+        # Cancel button
+        bnLogin = Button(self,
+                         text='Annuler',
+                         command=self.cancel)
+        bnLogin.grid(row=4, column=1, sticky=SE) # TODO : better snap to grid
 
     def login(self):
+        """ Renvoie False en cas d'échec, ou le nom d'utilisateur sinon. """
         loginInfo = {
-            'host'      : self.tbHost.get().strip(),
-            'database'  : self.tbDB.get().strip()
+            'user'      : self.tbUser.get().strip(),
+            'password'  : self.tbPW.get().strip()
             }
         print('DEBUG :\n' + str(loginInfo))
+        
+        with open('connexion_info.txt') as loginInfo:
+            host=loginInfo.readline().split(':')[1].strip()
+            database=loginInfo.readline().split(':')[1].strip()
+            user=loginInfo.readline().split(':')[1].strip()
+            password=loginInfo.readline().split(':')[1].strip()
+            self.model = Model(host, database, user, password)
+
+        if loginInfo['password'] == self.model.query("""
+            SELECT password
+            FROM VISITEUR
+            WHERE login = {}
+            """.format(loginInfo['user'])) :
+            res = loginInfo['user']
+        else:
+            res = False
+
+        return res
+
+    def cancel(self):
+        exit(0)
 
 
 # Test :
